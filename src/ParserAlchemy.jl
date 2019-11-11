3143939fe93020dea39b59e01d8a0c9160c1c91e
Gregor Kappler <gregor.kappler@bictxt.de>
2019-11-11 20:53:12 +0100

    UTF8 footnotes

--- src/ParserAlchemy.jl
+++ src/ParserAlchemy.jl
@@ -967,7 +967,8 @@ parenthesisP(open,close) = seq(String,
     open, r"[^][{}()]*", close;
     transform=(v,i) -> join(v))
 delimiter   = r"[-, _/\.;:*]+"
-word        = r"[^\[\]\(\){<>},*;:=\| \t_/\.\n\r\"'`]+"
+word        = r"[^\[\]\(\){<>},*;:=\| \t_/\.\n\r\"'`⁰¹²³⁴⁵⁶⁷⁸⁹]+"
+footnote    = r"^[⁰¹²³⁴⁵⁶⁷⁸⁹]+"
 enum_label = r"(?:[0-9]{1,3}|[ivx]{1,6}|[[:alpha:]])[\.\)]"
 
 
