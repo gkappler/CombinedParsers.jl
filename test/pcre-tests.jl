022da8f5784c7755091f48f2d6edcce625ac9b6a
Gregor Kappler <gregor.kappler@bictxt.de>
2020-05-30 14:25:41 +0200

    pcre-tests produce compliance report in manual

--- test/pcre-tests.jl
+++ test/pcre-tests.jl
@@ -6,20 +6,39 @@ include(joinpath(testdir, "pcretest-parser.jl"))
 tests_string=read(joinpath(testdir,"testoutput1"),String);
 tests = parse(tests_parser, tests_string)[1];
 
-ignore_idx = Dict{Int,String}(
-    14 => "",          ## conditions
-    69 => "Testing a PCRE regex for PCRE regex syntax. Omitted in test because of very long compile time.",
-    70 => "Testing a PCRE regex for PCRE regex syntax. Omitted in test because of very long compile time.", ## DEFINE
-    135 => "",
-    210 => "",
-    664 => "Omitted in test because of very long compile time.", ## slooow
-    706 => "Omitted in test because of very long compile time.", ## slooow
-    887 => "do I misunderstand recursive backreferences https://www.pcre.org/original/doc/html/pcrepattern.html#SEC19?",
-    1139 => "todo: ignorecase in backreference",
-    1146 => "case-ignoring backreferences",
-    1139 => "DUPNAMES",
-    1138 => "DUPNAMES",
-    1173 => ""
+tt=tests[14]
+@pcre_testset tt[2] true
+
+ignore_idx = SortedDict{Int,String}(
+    14 => "unicode escape in test parser needs to support \"\\u81\".",
+    69 => "because of very long compile time. The complex pattern parses email adresses",
+    70 => "because of very long compile time. The complex pattern parses email adresses",
+    89 => "CombinedParsers brackets use unicode character ranges. Avoid such patterns for now, contributions very welcome.",
+    90 => "CombinedParsers brackets use unicode character ranges. Avoid such patterns for now, contributions very welcome.",
+    135 => "escaped characters needs investigation.",
+    664 => "because of very long matching time for `Repeat(Repeat1('a'))` when there is no match. Avoid such patterns, contributions optimizing these cases are also very welcome.", ## slooow
+    887 => """do I misunderstand [recursive backreferences](https://www.pcre.org/original/doc/html/pcrepattern.html#SEC19)?
+
+```@repl session
+pattern = "(?P<abn>(?P=abn)xxx|)+"
+match(Regex(pattern),"xxx")
+match(Regcomb(pattern),"xxx")
+```
+
+IMO the pcre behaviour is confusing after considering the logic of expanding the repeat
+```@repl session
+pattern = "(?P<abn>(?P=abn)xxx|)((?P=abn)xxx|)"
+match(Regex(pattern),"xxx")
+match(Regcomb(pattern),"xxx")
+```
+
+""",
+    104 => "case-ignoring backreferences still need to be done, contributions are very welcome,",
+    260 => "case-ignoring backreferences still need to be done, contributions are very welcome,",
+    261 => "case-ignoring backreferences still need to be done, contributions are very welcome,",
+    562 => "case-ignoring backreferences still need to be done, contributions are very welcome,",
+    1146 => "case-ignoring backreferences still need to be done, contributions are very welcome,",
+    1173 => "because of very long compile time. The complex pattern parses PCRE regex syntax."
 )
 
 optimize_idx = []
@@ -75,7 +94,9 @@ testresults = @testset CustomTestSet "pcre testset 1" begin
                 end
             end
         end
-        if !isempty(tr.results)
+        nfailed = sum([ !(t isa Pass) for t in tr.results ])
+
+        if nfailed>0
             @warn "error in $i $(tt[2].pattern)"
             global err_idx,err_test=i,tt[2]
             global errors
@@ -113,69 +134,99 @@ io=stdout
 function print_testset(io,prefix,t,testdef; pad="   ")
     pat,opt = t.pattern
     pat = replace(pad*"/"*pat, "\n" => "\n"*pad)*"/"*opt
-    println(io,"$pad```\n$pat\n$pad```\n$pad$prefix\n")
-    testseqs = vcat([ "`"*s.sequence*"`" for s in t.test],"`".*t.tests_nomatch .* "`, no match")
+    println(io,"\n---\n$pad```\n$pat\n$pad```\n$pad$prefix")
+    testseqs = vcat([ "`"*s.sequence*"`" for s in t.test],"`".*t.tests_nomatch .* "` no match")
     for (j,tr) in enumerate(testdef.results)
         if (j>1)
             s = replace.(testseqs[j-1], "\n" => "\\n")
-            result = tr isa Pass ? "ok" : "failed"
-            println(io,pad*"- $s $result")
+            result = tr isa Pass ? "☑" : "☐"
+            println(io,pad*"- $result $s")
         end
     end
 end
 
 open(joinpath(docdir,"man","pcre-compliance.md"),"w") do io
-
   println(io,"""
 # Compliance with the PCRE test set
+!!! note 
+    The `@re_str` supports the following PCRE features
+    - ☑ fundamentals: sequences, alternations, repetitions optional, matches (`*`,`+`,`{n}`, `{min,}`, `{min,max}`, `?`)
+    - ☑ escaped characters and generic character types
+    - ☑ character ranges (`[]`)
+    - ☑ non-capturing groups,
+    - ☑ capturing groups, backreferences, subroutines (all by index, relative index and name)
+    - ☑ atomic groups
+    - ☑ lazy repetitions
+    - ☑ conditional expressions
+    - ☑ internal and pattern options setting
+    - ☑ simple assertions (`\\A`, `\\z`, `\\Z`, `\\b`, `\\B`, `^`, `\$`), 
+    - ☑ lookaheads and lookbehinds
+    - ☑ comments
+    PCRE functionality that is currently not supported:
+    - ☐ capture groups in lookbehinds.
+    - ☐ ACCEPT, SKIP, COMMIT, THEN, PRUNE, \\K
+```@setup session
+using CombinedParsers
+using CombinedParsers.Regexp
+```
 The test set is downloaded from [the PCRE source repository](https://github.com/rurban/pcre/blob/master/testdata/testoutput1).
 The PCRE test output is parsed with [a `CombinedParser`](https://github.com/gkappler/CombinedParsers.jl/blob/master/test/pcretest-parser.jl).
+## Test Overview
+$n_successes successful tests on $(n_patterns.success) patterns
+(See [list of compliant patterns](pcre-compliance-succeeded.html)).\n
+$n_failed failed tests on $(n_patterns.failed) patterns
+(See [list of failed patterns](pcre-compliance-failed.html)).
+### Performance Overview:
+CombinedParsers is a very young package that will be optimized further, 
+but already `@re_str` pure Julia regular expression parsing is competitive with `@r_str` with the PCRE C backend which has arrived at a widely optimized codebase after decades of improvements.\n
+Benchmark timings for regular expression construction and matching comparing `Regex` (x axis) and `Regcomb` (y axis), both on a log10 scale:\n\n
+![](log_btimes.png)\n\n
+Points represent PCRE an individual benchmark. 
+PCRE benchmarks have a range between 82ns to 518ns.
+CombinedParsers benchmarks have a ~20-times larger range between 33ns to 140μs.
+### Benchmark ratios histogram:
+![](log_btime_ratio_histogram.svg)\n\n
 
-## Overview
-$n_successes successful tests on $(n_patterns.success) patterns.
-$n_failed failed tests on $(n_patterns.failed) patterns.
+The equal are histograms of log10-ratios of `time_Regcomb/time_Regex` shows that `CombinedParser` implementation is competitive, with speeds up to 10x faster in best cases and only rarely 100x slower in worst cases.
+Worst cases are investigated for improving [in this IJulia notebook](...).
 
-##### Performance Overview:
-
-Points represent PCRE pattern tests. The axis represent benchmark times with 
-`Regex` (x axis) and `Regcomb` (y axis), both on a log10 scale.
-
-
-
-##### Benchmark ratios histogram:
-
-### Unsupported
-
+## Unsupported
 $(n_patterns.unsupported) unsupported patterns were omitted for the following reasons:
 """)
-
     for (n,r) in sort([ s.first => length(s.second) for s in unsupported ])
-        println(io,"- `$n` failed on $r")
+        println(io,"- `$n` failed on $r patterns.")
     end
-
-    println(io,"""
-
-### Skipped
+    println(io,"""\n
+## Skipped
 $(n_patterns.skipped) patterns were skipped for the following reasons:
 """)
     for (i,r) in ignore_idx
         t = tests[i][2]
-        print_testset(io,"(#$i) skipped, $r.\n",t,testresults.results[i], pad="")
+        pat,opt = t.pattern
+        @show lastindex(pat)
+        if lastindex(pat)>100
+            print(io,"\n\n(#$i) skipped, $r\n")
+        else
+            print_testset(io,"(#$i) skipped, $r\n",t,testresults.results[i], pad="")
+        end
     end
+end
 
-    println(io,"""
-
-## Failed Tests
+open(joinpath(docdir,"man","pcre-compliance-failed.md"),"w") do io
+    println(io,"""\n
+# Failed PCRE Tests
 $n_failed failed tests on $(n_patterns.failed) patterns.
 """)
-    
     for (i,r) in failures
         t = tests[i][2]
-        print_testset(io,"(#$i) failed $r times:\n",t,testresults.results[i], pad="")
+        print_testset(io,"(#$i) failed $r times:",t,testresults.results[i], pad="")
     end
+end
+
+open(joinpath(docdir,"man","pcre-compliance-succeeded.md"),"w") do io
 
     println(io,"""
-## Compliance examples
+# PCRE Compliance
 $n_successes successful tests on $n_patterns.success patterns.
 
 """)
