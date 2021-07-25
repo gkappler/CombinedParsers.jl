8ca1d07e5ca1787fcbd78178dab496418fc4ca71
Gregor Kappler <gregor@x360.fritz.box>
2021-07-25 13:27:59 +0200

    fix dates tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,14 @@ using Test
 end
 
 import TextParse
+import TextParse: Numeric
 import Dates
 @testset "TextParse" begin
-    @test Numeric(Int) isa CombinedParser
+    @test parser(Numeric(Int)) isa CombinedParser
     @test !(TextParse.Numeric(Int) isa CombinedParser)
     @test parse(("some " * Numeric(Int))[2], "some 100") == 100
     Dates.DateFormat.(("y-m-d","d.m.y"))
-    CombinedParsers.Date("y-m-d","d.m.y")
+    CombinedParsers.DateParser("y-m-d","d.m.y")
     schedule = ("on " * CombinedParsers.DateParser("y-m-d","d.m.y"))[2]
     @test parse(schedule, "on 12.12.2020") == Dates.Date(2020,12,12)
     @test parse(schedule, "on 2020-12-12") == Dates.Date(2020,12,12)
