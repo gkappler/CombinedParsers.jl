b32b0c7c31126c65eea09dd6fee30e6f2e8a1540
Gregor Kappler <gregor@x360.fritz.box>
2021-08-06 23:05:22 +0200

    StringWrapper

--- src/StringWrappers.jl
+++ src/StringWrappers.jl
@@ -1,38 +1,39 @@
 """
     abstract type StringWrapper <: AbstractString end
 
-Provides default `@inline` AbstractString API methods defering to property `:x`.
+Provides default `sw::StringWrapper<:AbstractString` API methods defering to `wrappedstring(sw)` call, by default `sw.x`.
 """
 abstract type StringWrapper <: AbstractString end
-@inline Base.@propagate_inbounds Base.getindex(x::StringWrapper,i::Integer) =
-    getindex(x.x,i)
-@inline Base.@propagate_inbounds Base.iterate(x::StringWrapper) =
-    iterate(x.x)
-@inline Base.@propagate_inbounds Base.iterate(x::StringWrapper,i::Integer) =
-    iterate(x.x,i)
-@inline Base.@propagate_inbounds Base.SubString(x::StringWrapper,start::Int,stop::Int) =
-    SubString(x.x,start,stop)
-@inline Base.@propagate_inbounds Base.length(x::StringWrapper) =
-    length(x.x)
-@inline Base.@propagate_inbounds Base.lastindex(x::StringWrapper) =
-    lastindex(x.x)
-@inline Base.@propagate_inbounds Base.firstindex(x::StringWrapper) =
-    firstindex(x.x)
-@inline Base.@propagate_inbounds Base.prevind(x::StringWrapper,i::Int,n::Int) =
-    prevind(x.x,i,n)
-@inline Base.@propagate_inbounds Base.nextind(x::StringWrapper,i::Int,n::Int) =
-    nextind(x.x,i,n)
-@inline Base.@propagate_inbounds Base.prevind(x::StringWrapper,i::Int) =
-    prevind(x.x,i)
-@inline Base.@propagate_inbounds Base.nextind(x::StringWrapper,i::Int) =
-    nextind(x.x,i)
-@inline Base.@propagate_inbounds Base.ncodeunits(x::StringWrapper) =
-    ncodeunits(x.x)
+@inline wrappedstring(sw::StringWrapper) = sw.x
+@inline Base.@propagate_inbounds Base.getindex(sw::StringWrapper,i::Integer) =
+    getindex(wrappedstring(sw),i)
+@inline Base.@propagate_inbounds Base.iterate(sw::StringWrapper) =
+    iterate(wrappedstring(sw))
+@inline Base.@propagate_inbounds Base.iterate(sw::StringWrapper,i::Integer) =
+    iterate(wrappedstring(sw),i)
+@inline Base.@propagate_inbounds Base.SubString(sw::StringWrapper,start::Int,stop::Int) =
+    SubString(wrappedstring(sw),start,stop)
+@inline Base.@propagate_inbounds Base.length(sw::StringWrapper) =
+    length(wrappedstring(sw))
+@inline Base.@propagate_inbounds Base.lastindex(sw::StringWrapper) =
+    lastindex(wrappedstring(sw))
+@inline Base.@propagate_inbounds Base.firstindex(sw::StringWrapper) =
+    firstindex(wrappedstring(sw))
+@inline Base.@propagate_inbounds Base.prevind(sw::StringWrapper,i::Int,n::Int) =
+    prevind(wrappedstring(sw),i,n)
+@inline Base.@propagate_inbounds Base.nextind(sw::StringWrapper,i::Int,n::Int) =
+    nextind(wrappedstring(sw),i,n)
+@inline Base.@propagate_inbounds Base.prevind(sw::StringWrapper,i::Int) =
+    prevind(wrappedstring(sw),i)
+@inline Base.@propagate_inbounds Base.nextind(sw::StringWrapper,i::Int) =
+    nextind(wrappedstring(sw),i)
+@inline Base.@propagate_inbounds Base.ncodeunits(sw::StringWrapper) =
+    ncodeunits(wrappedstring(sw))
 @inline Base.@propagate_inbounds Base.codeunit(s::StringWrapper, i::Integer) =
-    codeunit(x.x, i)
-@inline Base.@propagate_inbounds Base.isvalid(x::StringWrapper,i::Int) =
-    isvalid(x.x, i)
-@inline Base.@propagate_inbounds Base.thisind(x::StringWrapper,i::Int) =
-    thisind(x.x, i)
+    codeunit(wrappedstring(sw), i)
+@inline Base.@propagate_inbounds Base.isvalid(sw::StringWrapper,i::Int) =
+    isvalid(wrappedstring(sw), i)
+@inline Base.@propagate_inbounds Base.thisind(sw::StringWrapper,i::Int) =
+    thisind(wrappedstring(sw), i)
 
 
