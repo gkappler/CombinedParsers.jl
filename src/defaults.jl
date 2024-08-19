7172102e638b879bd0dcbb03d8ab001fd6e4db43
Gregor Kappler <mail@g-kappler.de>
2024-08-19 16:11:19 +0200

    Refactor space and whitespace related parsers into functions to improve readability and maintainability
    - Replaced `const` declarations of space and whitespace parsers with functions.
    - Introduced a new `@defaults` macro to define default parsers and export them.
    - Enhanced documentation for various imported and newly defined functions and macros.
    - Optimized and reorganized `trim` and `@trimmed` functions for improved whitespace handling.

--- src/defaults.jl
+++ src/defaults.jl
@@ -1,157 +1,34 @@
-export whitespace_char, whitespace_maybe, whitespace
-export horizontal_space_char, horizontal_space_maybe, horizontal_space
-export vertical_space_char, vertical_space_maybe, vertical_space
-export space, space_maybe, whitespace_newline
-export trim, @trimmed
-export newline, inline
-export at_linestart, at_lineend
+export @defaults
+export integer_base
 
-char_label = Dict(
-    '\U0009' => "Horizontal tab (HT)",
-    '\U0020' => "Space",
-    '\U00A0' => "Non-break space",
-    '\U1680' => "Ogham space mark",
-    '\U180E' => "Mongolian vowel separator",
-    '\U2000' => "En quad",
-    '\U2001' => "Em quad",
-    '\U2002' => "En space",
-    '\U2003' => "Em space",
-    '\U2004' => "Three-per-em space",
-    '\U2005' => "Four-per-em space",
-    '\U2006' => "Six-per-em space",
-    '\U2007' => "Figure space",
-    '\U2008' => "Punctuation space",
-    '\U2009' => "Thin space",
-    '\U200A' => "Hair space",
-    '\U202F' => "Narrow no-break space",
-    '\U205F' => "Medium mathematical space",
-    '\U3000' => "Ideographic space",
-    '\u2028' => "Line Separator",
-    '\u200e' => "Left-to-right mark",
-    '\v' => "Vertical tab",
-    '\u2029' => "Paragraph separator",
-    '\u85' => "Next line",
-    '\u200f' => "Right-to-left mark",
-    '\f' => "Form feed",
-    '\U000A' => "Linefeed (LF)",
-    '\U000B' => "Vertical tab (VT)",
-    '\U000C' => "Form feed (FF)",
-    '\U000D' => "Carriage return (CR)",
-    '\U0085' => "Next line (NEL)",
-    '\U2028' => "Line separator",
-    '\U2029' => "Paragraph separator"
-)
 
-char_label_table(x::Union{ValueIn,ValueNotIn}) =
-    char_label_table(x.sets)
-function char_label_table(x)
-    chars = sort(collect(x))
-    rs = [ escape_string(repr(c)) for c in chars ]
-    labels = [ get(char_label, c, "") for c in chars ]
-    nr = maximum(length.(rs))
-    n = maximum(length.(labels))
-    println("| ",lpad("Char",nr)," | ", lpad("",n), " |")
-    println("|-","-"^nr, "-|-", "-"^n,      "-|")
-    for (c,l) in zip(rs, labels)
-        print("| ")
-        print(lpad(c,nr))
-        println(" | ", lpad(l,n), " |")
+hex_digit() = CharIn("[:xdigit:]",'A':'F','a':'f','0':'9')
+
+"""
+    integer_base(base,mind=1,maxd=Repeat_max)
+
+Parser matching a integer format on base `base`.
+
+!!! note
+    Uses a second Base.parse call on match.
+    
+    A custom parser could aggregate result incrementally while matching.
+"""
+function integer_base(base=10,mind=1,maxd=Repeat_max)
+    dig = if base == 16
+        hex_digit()
+    elseif base <= 10
+        ValueIn('0':('0'+(base-1)))
+    else
+        error("Base $base not supported")
+    end
+    map(!Repeat(mind:maxd,dig)) do v
+        (isempty(v) ? 0 : parse(Int,convert(String,v),base=base))::Int
     end
 end
 
-const whitespace_char = CharIn(
-    "[:space:]",
-    " \t\U0085\U200E\U200F\U2028\U2029"*"\U2029\U000C\U000B")
 
-const whitespace_maybe = !Atomic(Repeat(whitespace_char))
-const whitespace = !Atomic(Repeat1(whitespace_char))
-
-"""
-    whitespace_char  = re"[[:space:]]"
-    whitespace_maybe = re"(?>[[:space:]]*)"
-    whitespace       = re"(?>[[:space:]]+)"
-
-```jldoc
-julia> CombinedParsers.char_label_table(whitespace_char)
-|      Char |                     |
-|-----------|---------------------|
-|     '\\t' | Horizontal tab (HT) |
-|     '\\v' |   Vertical tab (VT) |
-|     '\\f' |      Form feed (FF) |
-|       ' ' |               Space |
-|   '\\u85' |     Next line (NEL) |
-| '\\u200e' |  Left-to-right mark |
-| '\\u200f' |  Right-to-left mark |
-| '\\u2028' |      Line separator |
-| '\\u2029' | Paragraph separator |
-```
-"""
-whitespace_char, whitespace_maybe, whitespace
-
-
-const horizontal_space_char=CharIn("\\h",
-    '\U0009', # "Horizontal tab (HT)"),
-    '\U0020', # "Space"),
-    '\U00A0', # "Non-break space"),
-    '\U1680', # "Ogham space mark"),
-    '\U180E', # "Mongolian vowel separator"),
-    '\U2000', # "En quad"),
-    '\U2001', # "Em quad"),
-    '\U2002', # "En space"),
-    '\U2003', # "Em space"),
-    '\U2004', # "Three-per-em space"),
-    '\U2005', # "Four-per-em space"),
-    '\U2006', # "Six-per-em space"),
-    '\U2007', # "Figure space"),
-    '\U2008', # "Punctuation space"),
-    '\U2009', # "Thin space"),
-    '\U200A', # "Hair space"),
-    '\U202F', # "Narrow no-break space"),
-    '\U205F', # "Medium mathematical space"),
-    '\U3000' # "Ideographic space"))
-)
-
-const horizontal_space_maybe = with_name(:horizontal_space_maybe,
-                                   !Atomic(Repeat(horizontal_space_char)),
-                                   "\\h*")
-const horizontal_space = with_name(:horizontal_space,
-                             !Atomic(Repeat1(horizontal_space_char)),
-                             "\\h+")
-
-
-"""
-    horizontal_space_char  = re"[\\h]"
-    horizontal_space_maybe = re"(?>[\\h]*)"
-    horizontal_space       = re"(?>[\\h]+)"
-
-```jldoc
-julia> CombinedParsers.char_label_table(horizontal_space_char)
-|      Char |                           |
-|-----------|---------------------------|
-|     '\\t' |       Horizontal tab (HT) |
-|       ' ' |                     Space |
-|       ' ' |           Non-break space |
-|       ' ' |          Ogham space mark |
-| '\\u180e' | Mongolian vowel separator |
-|       ' ' |                   En quad |
-|       ' ' |                   Em quad |
-|       ' ' |                  En space |
-|       ' ' |                  Em space |
-|       ' ' |        Three-per-em space |
-|       ' ' |         Four-per-em space |
-|       ' ' |          Six-per-em space |
-|       ' ' |              Figure space |
-|       ' ' |         Punctuation space |
-|       ' ' |                Thin space |
-|       ' ' |                Hair space |
-|       ' ' |     Narrow no-break space |
-|       ' ' | Medium mathematical space |
-|       '　' |         Ideographic space |
-```
-"""
-horizontal_space_char, horizontal_space_maybe, horizontal_space
-
-const whitespace_horizontal = horizontal_space
+export trim, @trimmed
 
 """
     trim(p...; whitespace=horizontal_space_maybe, 
@@ -159,7 +36,7 @@ const whitespace_horizontal = horizontal_space
 
 Ignore whitespace `left` and `right` of `sSequence(p...)`.
 """
-trim(p...; whitespace=horizontal_space_maybe, left=whitespace, right=whitespace) =
+trim(p...; whitespace=horizontal_space_maybe(), left=whitespace, right=whitespace) =
     Sequence(left, sSequence(p...), right)[2]
 
 
@@ -207,7 +84,205 @@ macro trimmed(block)
     esc(trimmed(block))
 end
 
-const vertical_space_char=CharIn(
+
+"""
+    @defaults
+
+Define a set of default whitespace and spacing-related parsers from the `CombinedParsers` module.
+
+The macro establishes the following parsers:
+
+- `whitespace_char`: Parser for a single whitespace character.
+- `whitespace_maybe`: Parser for optional whitespace.
+- `whitespace`: Parser for one or more whitespace characters.
+- `horizontal_space_char`: Parser for a single horizontal space character.
+- `horizontal_space_maybe`: Parser for optional horizontal space.
+- `horizontal_space`: Parser for one or more horizontal space characters.
+- `vertical_space_char`: Parser for a single vertical space character.
+- `vertical_space_maybe`: Parser for optional vertical space.
+- `vertical_space`: Parser for one or more vertical space characters.
+- `space`: Parser for a general space.
+- `space_maybe`: Parser for optional space.
+- `whitespace_newline`: Parser for a newline in the context of whitespace.
+- `newline`: Parser for a newline character.
+- `inline`: Parser for inline content.
+- `at_linestart`: Parser for the start of a line.
+- `at_lineend`: Parser for the end of a line.
+- `hex_digit`: Parser for a hexadecimal digit.
+"""
+macro defaults()
+    esc(quote
+            whitespace_char = CombinedParsers.whitespace_char()
+            whitespace_maybe = CombinedParsers.whitespace_maybe()
+            whitespace = CombinedParsers.whitespace()
+            horizontal_space_char = CombinedParsers.horizontal_space_char()
+            horizontal_space_maybe = CombinedParsers.horizontal_space_maybe()
+            horizontal_space = CombinedParsers.horizontal_space()
+            whitespace_horizontal = CombinedParsers.whitespace_horizontal()
+            vertical_space_char = CombinedParsers.vertical_space_char()
+            vertical_space_maybe = CombinedParsers.vertical_space_maybe()
+            vertical_space = CombinedParsers.vertical_space()
+            space = CombinedParsers.space()
+            space_maybe = CombinedParsers.space_maybe()
+            newline = CombinedParsers.newline()
+            inline = CombinedParsers.inline()
+            at_linestart = CombinedParsers.at_linestart()
+            at_lineend = CombinedParsers.at_lineend()
+            hex_digit = CombinedParsers.hex_digit()
+            word = CombinedParsers.word()
+            non_word = CombinedParsers.non_word()
+        end
+        )
+end
+
+
+char_label_table(x::Union{ValueIn,ValueNotIn}) =
+    char_label_table(x.sets)
+function char_label_table(x)
+    char_label = Dict(
+        '\U0009' => "Horizontal tab (HT)",
+        '\U0020' => "Space",
+        '\U00A0' => "Non-break space",
+        '\U1680' => "Ogham space mark",
+        '\U180E' => "Mongolian vowel separator",
+        '\U2000' => "En quad",
+        '\U2001' => "Em quad",
+        '\U2002' => "En space",
+        '\U2003' => "Em space",
+        '\U2004' => "Three-per-em space",
+        '\U2005' => "Four-per-em space",
+        '\U2006' => "Six-per-em space",
+        '\U2007' => "Figure space",
+        '\U2008' => "Punctuation space",
+        '\U2009' => "Thin space",
+        '\U200A' => "Hair space",
+        '\U202F' => "Narrow no-break space",
+        '\U205F' => "Medium mathematical space",
+        '\U3000' => "Ideographic space",
+        '\u2028' => "Line Separator",
+        '\u200e' => "Left-to-right mark",
+        '\v' => "Vertical tab",
+        '\u2029' => "Paragraph separator",
+        '\u85' => "Next line",
+        '\u200f' => "Right-to-left mark",
+        '\f' => "Form feed",
+        '\U000A' => "Linefeed (LF)",
+        '\U000B' => "Vertical tab (VT)",
+        '\U000C' => "Form feed (FF)",
+        '\U000D' => "Carriage return (CR)",
+        '\U0085' => "Next line (NEL)",
+        '\U2028' => "Line separator",
+        '\U2029' => "Paragraph separator"
+    )
+    chars = sort(collect(x))
+    rs = [ escape_string(repr(c)) for c in chars ]
+    labels = [ get(char_label, c, "") for c in chars ]
+    nr = maximum(length.(rs))
+    n = maximum(length.(labels))
+    println("| ",lpad("Char",nr)," | ", lpad("",n), " |")
+    println("|-","-"^nr, "-|-", "-"^n,      "-|")
+    for (c,l) in zip(rs, labels)
+        print("| ")
+        print(lpad(c,nr))
+        println(" | ", lpad(l,n), " |")
+    end
+end
+
+whitespace_char() = CharIn(
+    "[:space:]",
+    " \t\U0085\U200E\U200F\U2028\U2029"*"\U2029\U000C\U000B")
+
+whitespace_maybe() = !Atomic(Repeat(whitespace_char()))
+whitespace() = !Atomic(Repeat1(whitespace_char()))
+
+"""
+    whitespace_char  = re"[[:space:]]"
+    whitespace_maybe = re"(?>[[:space:]]*)"
+    whitespace       = re"(?>[[:space:]]+)"
+
+```jldoc
+julia> CombinedParsers.char_label_table(whitespace_char)
+|      Char |                     |
+|-----------|---------------------|
+|     '\\t' | Horizontal tab (HT) |
+|     '\\v' |   Vertical tab (VT) |
+|     '\\f' |      Form feed (FF) |
+|       ' ' |               Space |
+|   '\\u85' |     Next line (NEL) |
+| '\\u200e' |  Left-to-right mark |
+| '\\u200f' |  Right-to-left mark |
+| '\\u2028' |      Line separator |
+| '\\u2029' | Paragraph separator |
+```
+"""
+whitespace_char, whitespace_maybe, whitespace
+
+
+horizontal_space_char() = CharIn("\\h",
+    '\U0009', # "Horizontal tab (HT)"),
+    '\U0020', # "Space"),
+    '\U00A0', # "Non-break space"),
+    '\U1680', # "Ogham space mark"),
+    '\U180E', # "Mongolian vowel separator"),
+    '\U2000', # "En quad"),
+    '\U2001', # "Em quad"),
+    '\U2002', # "En space"),
+    '\U2003', # "Em space"),
+    '\U2004', # "Three-per-em space"),
+    '\U2005', # "Four-per-em space"),
+    '\U2006', # "Six-per-em space"),
+    '\U2007', # "Figure space"),
+    '\U2008', # "Punctuation space"),
+    '\U2009', # "Thin space"),
+    '\U200A', # "Hair space"),
+    '\U202F', # "Narrow no-break space"),
+    '\U205F', # "Medium mathematical space"),
+    '\U3000' # "Ideographic space"))
+)
+
+horizontal_space_maybe() = with_name(:horizontal_space_maybe,
+                                   !Atomic(Repeat(horizontal_space_char())),
+                                   "\\h*")
+horizontal_space() = with_name(:horizontal_space,
+                             !Atomic(Repeat1(horizontal_space_char())),
+                             "\\h+")
+
+
+"""
+    horizontal_space_char  = re"[\\h]"
+    horizontal_space_maybe = re"(?>[\\h]*)"
+    horizontal_space       = re"(?>[\\h]+)"
+
+```jldoc
+julia> CombinedParsers.char_label_table(horizontal_space_char)
+|      Char |                           |
+|-----------|---------------------------|
+|     '\\t' |       Horizontal tab (HT) |
+|       ' ' |                     Space |
+|       ' ' |           Non-break space |
+|       ' ' |          Ogham space mark |
+| '\\u180e' | Mongolian vowel separator |
+|       ' ' |                   En quad |
+|       ' ' |                   Em quad |
+|       ' ' |                  En space |
+|       ' ' |                  Em space |
+|       ' ' |        Three-per-em space |
+|       ' ' |         Four-per-em space |
+|       ' ' |          Six-per-em space |
+|       ' ' |              Figure space |
+|       ' ' |         Punctuation space |
+|       ' ' |                Thin space |
+|       ' ' |                Hair space |
+|       ' ' |     Narrow no-break space |
+|       ' ' | Medium mathematical space |
+|       '　' |         Ideographic space |
+```
+"""
+horizontal_space_char, horizontal_space_maybe, horizontal_space
+
+whitespace_horizontal() = horizontal_space()
+
+vertical_space_char() = CharIn(
     "\\v",
     '\U000A', # "Linefeed (LF)"),
     '\U000B', # "Vertical tab (VT)"),
@@ -217,11 +292,11 @@ const vertical_space_char=CharIn(
     '\U2028', # "Line separator"),
     '\U2029') # "Paragraph separator"))
 
-const vertical_space_maybe = with_name(:vertical_space_maybe,
-                                 !Atomic(Repeat(vertical_space_char)),
+vertical_space_maybe() = with_name(:vertical_space_maybe,
+                                 !Atomic(Repeat(vertical_space_char())),
                                  "\\v*")
-const vertical_space = with_name(:vertical_space,
-                           !Atomic(Repeat1(vertical_space_char)),
+vertical_space() = with_name(:vertical_space,
+                           !Atomic(Repeat1(vertical_space_char())),
                            "\\v+")
 
 """
@@ -246,10 +321,10 @@ vertical_space_char, vertical_space_maybe, vertical_space
 
 
 "Equivalent PRCE `\\h\\v`, [`horizontal_space_char`](@ref), [`vertical_space_char`](@ref)"
-const space_char  = CharIn("\\h\\v",horizontal_space_char,vertical_space_char)
+space_char() = CharIn("\\h\\v",horizontal_space_char(),vertical_space_char())
 
-const space_maybe = Atomic(!Repeat(space_char))
-const space = Atomic(!Repeat1(CharIn("\\h\\v",space_char)))
+space_maybe() = Atomic(!Repeat(space_char()))
+space() = Atomic(!Repeat1(CharIn("\\h\\v",space_char())))
 
 """
     CombinedParsers.newline
@@ -270,28 +345,28 @@ julia> CombinedParsers.bsr.parser
 ```
 
 """
-const bsr = with_name(
+bsr() = with_name(
     :bsr, !Atomic(Either("\r\n",
                         CharIn(raw"\n\x0b\f\r\x85", '\n','\x0b','\f','\r','\U0085', '\U2028','\U2029'))),
     "\\r");
 
-const newline = bsr
+newline() = bsr()
 
-const alpha = CharIn('a':'z','A':'Z')
-const alphanum = CharIn('a':'z','A':'Z','0':'9')
+alpha() = CharIn('a':'z','A':'Z')
+alphanum() = CharIn('a':'z','A':'Z','0':'9')
 
 "Equivalent PRCE `\\w`: Char with unicode class `L`, `N`, or `_`."
-const word_char=CharIn("\\w",UnicodeClass("L","N"),'_')
+word_char() = CharIn("\\w",UnicodeClass("L","N"),'_')
 
 "SubString of at least 1 repeated [`CombinedParsers.word_char`](@ref)."
-const word = with_name(:word, !Repeat1(word_char), ## "[[:alpha:] ]+"
+word() = with_name(:word, !Repeat1(word_char()), ## "[[:alpha:] ]+"
                  "\\w+")
 
 "Vector of at least 1 repeated [`CombinedParsers.word`](@ref)s delimited by [`CombinedParsers.whitespace_horizontal`](@ref)."
-const words = join(word, whitespace_horizontal) ## "[[:alpha:] ]+"
+words() = join(word, whitespace_horizontal()) ## "[[:alpha:] ]+"
 
-const non_word_char=CharNotIn("\\W",UnicodeClass("L","N"),'_')
-const non_word = with_name(:non_word, !Repeat1(non_word_char),
+non_word_char() = CharNotIn("\\W",UnicodeClass("L","N"),'_')
+non_word() = with_name(:non_word, !Repeat1(non_word_char()),
                      "\\W+")## "[[:alpha:] ]+"
 
 """
@@ -299,14 +374,14 @@ const non_word = with_name(:non_word, !Repeat1(non_word_char),
 
 Parser part of `word_boundary`.
 """
-const beyond_word = Either(non_word_char,AtStart(),AtEnd())
+beyond_word() = Either(non_word_char(),AtStart(),AtEnd())
 
 """
     word_boundary = re"\b"
 """
-const word_boundary = Either(
-    Sequence(PositiveLookbehind(word_char),PositiveLookahead(beyond_word)),
-    Sequence(PositiveLookbehind(beyond_word),PositiveLookahead(word_char))
+word_boundary() = Either(
+    Sequence(PositiveLookbehind(word_char()),PositiveLookahead(beyond_word())),
+    Sequence(PositiveLookbehind(beyond_word()),PositiveLookahead(word_char()))
 )
 
 """
@@ -325,7 +400,7 @@ julia> CombinedParsers.at_linestart
 !!! note
     used in `re"^"` if `Base.PCRE.MULTILINE` is set.
 """
-const at_linestart = Either(AtStart(),PositiveLookbehind(bsr))
+at_linestart() = Either(AtStart(),PositiveLookbehind(bsr()))
 
 
 """
@@ -344,7 +419,7 @@ julia> CombinedParsers.at_lineend
 !!! note
     used in `re"\$"` if `Base.PCRE.MULTILINE` is set.
 """
-const at_lineend   = Either(AtEnd(),PositiveLookahead(bsr))
+at_lineend() = Either(AtEnd(),PositiveLookahead(bsr()))
 #@deprecate lineend at_lineend
 
 """
@@ -352,32 +427,7 @@ const at_lineend   = Either(AtEnd(),PositiveLookahead(bsr))
 
 See [`at_lineend`](@ref).
 """
-const inline = !Atomic(Repeat(NegativeLookahead(at_lineend)*AnyChar()))
+inline() = !Atomic(Repeat(NegativeLookahead(at_lineend())*AnyChar()))
 
 
-
-const hex_digit = CharIn("[:xdigit:]",'A':'F','a':'f','0':'9')
-
-"""
-    integer_base(base,mind=1,maxd=Repeat_max)
-
-Parser matching a integer format on base `base`.
-
-!!! note
-    Uses a second Base.parse call on match.
-    
-    A custom parser could aggregate result incrementally while matching.
-"""
-function integer_base(base=10,mind=1,maxd=Repeat_max)
-    dig = if base == 16
-        hex_digit
-    elseif base <= 10
-        ValueIn('0':('0'+(base-1)))
-    else
-        error("Base $base not supported")
-    end
-    map(!Repeat(mind:maxd,dig)) do v
-        (isempty(v) ? 0 : parse(Int,convert(String,v),base=base))::Int
-    end
-end
 
